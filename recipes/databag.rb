67c7353e9a6790c7a192b03791f7398a3a0f9ffc
James Kyle <james@jameskyle.org>
2013-01-04 18:03:00 -0800

    Incremented version. moved default snakeoil task into its own recipe (default behavior stays the same). Created deault attributes for disabling snakeoil and setting the databag to fetch certs from. Created databag recipe to fetch certs from enc databag. Now requires helpers cookbook. Looks up certs in default['ssl-cert']['databag']['name'] data bag. Data bag is a key=>value hash with key equal to path and value equal to cert.

---
+++ recipes/databag.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: ssl-cert
+# Recipe:: databag
+#
+# Copyright 2012, AT&T Foundry
+#
+# Licensed under the Apache License, Version 2.0
+
+bag_id = node["fqdn"].gsub(".","-")
+bag    = node['ssl-cert']['databag']['name']
+site   = encrypted_data_bag(bag, bag_id).to_hash
+site.delete('id')
+
+site.each do |path, cert|
+  file path do
+    owner "root"
+    group "root"
+    content cert  
+    mode 00644
+    action :create
+  end 
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,22 +4,8 @@
 #
 # Copyright 2012, AT&T Foundry
 #
-# All rights reserved - Do Not Redistribute
+# Licensed under the Apache License, Version 2.0
 
-case node[:platform]
-when "ubuntu", "debian"
-  package "ssl-cert" do
-    action :install
-  end
-
-  if not File.exists? "/etc/ssl/certs/ssl-cert-snakeoil.pem" then
-    script "install_something" do
-      interpreter "bash"
-      user "root"
-      code <<-EOH
-      /usr/sbin/make-ssl-cert generate-default-snakeoil
-      EOH
-    end
-  end
+if node['ssl-cert']['snakeoil'] then
+  include_recipe "ssl-cert::snakeoil"
 end
-
